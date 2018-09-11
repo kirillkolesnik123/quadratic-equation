ce430521392dc134d2270d1cba7b6b2c599dea3e
kirillkolesnik123 <42231774+kirillkolesnik123@users.noreply.github.com>
2018-09-11 17:22:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+   let data = equation.split(' ');
+	
+    let a = Number(data[0]);
+    
+    let b = Number(data[3] + data[4]);
+    
+    let c = Number(data[7] + data[8]);
+    
+
+    let d = (data[3] + data[4])*(data[3] + data[4]) - 4*(data[0] * (data[7] + data[8]));
+       
+    var x1 = Math.round((-b + Math.sqrt(d))/(2*a));
+    
+    var x2 = Math.round((-b - Math.sqrt(d))/(2*a));
+    
+
+   let arr = [x1, x2];
+   
+   function sortab(a,b ) {
+       return a - b;
+   }
+
+   return arr.sort(sortab);
 }
